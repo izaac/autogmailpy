17ae6109439845312c2e9162cf0089d9a75c1335
Izaac Zavaleta <jorge.izaac@gmail.com>
2015-02-02 11:00:15 -0700

    Removed temporary comments from tests

--- autogmailpy/tests/gmail_inbox_test.py
+++ autogmailpy/tests/gmail_inbox_test.py
@@ -33,26 +33,26 @@ class TestGmailInbox(unittest.TestCase):
         self.gbox = GmailInbox(self.driver)
         self.gbox.wait = self.wait
 
-    # @screenshot_on_error
-    # def test_compose_email(self):
-    #     self._setup_driver()
-    #     self.gbox.go_inbox()
-    #     self.gbox.body = 'This is the Email Body'
-    #     total_emails_before = self.gbox.get_inbox_total()
-    #     self.gbox.compose()
-    #     total_emails_after = self.gbox.get_inbox_total()
-    #     self.assertLess(total_emails_before, total_emails_after)
-    #
-    # @screenshot_on_error
-    # def test_validate_sent_item(self):
-    #     self._setup_driver()
-    #     self.gbox.go_inbox()
-    #     self.gbox.body = '{0}'.format(uuid.uuid4())
-    #     self.gbox.compose()
-    #     self.gbox.check_in_sent()
-    #     self.gbox.wait_for(self.gbox.first_email_entry_content())
-    #     first_sent_entry = self.gbox.first_email_entry_content().text
-    #     self.assertEqual(first_sent_entry[3:], self.gbox.body)
+    @screenshot_on_error
+    def test_compose_email(self):
+        self._setup_driver()
+        self.gbox.go_inbox()
+        self.gbox.body = 'This is the Email Body'
+        total_emails_before = self.gbox.get_inbox_total()
+        self.gbox.compose()
+        total_emails_after = self.gbox.get_inbox_total()
+        self.assertLess(total_emails_before, total_emails_after)
+
+    @screenshot_on_error
+    def test_validate_sent_item(self):
+        self._setup_driver()
+        self.gbox.go_inbox()
+        self.gbox.body = '{0}'.format(uuid.uuid4())
+        self.gbox.compose()
+        self.gbox.check_in_sent()
+        self.gbox.wait_for(self.gbox.first_email_entry_content())
+        first_sent_entry = self.gbox.first_email_entry_content().text
+        self.assertEqual(first_sent_entry[3:], self.gbox.body)
 
     @screenshot_on_error
     def test_validate_spam_delete(self):
