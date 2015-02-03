8791b8f5b4e9946cb08c9aa1cc6bf4e01ce7eb6f
Izaac Zavaleta <izavaleta@tripwire.com>
2015-02-03 16:19:58 -0700

    Classifiers added to the setup.py setuptools file.

--- setup.py
+++ setup.py
@@ -10,5 +10,10 @@ setup(
     author_email='jorge.izaac@gmail.com',
     description='Python Selenium Gmail Tests',
     test_suite='autogmailpy.tests',
-    install_requires=['selenium>=2.44.0']
+    install_requires=['selenium>=2.44.0'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
 )
